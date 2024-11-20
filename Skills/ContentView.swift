1b55c1a5a93836444fe67f64a8d188021a28e0a6
Kyle Rohr <public@antipex.com>
2024-11-20 11:21:42 -0800

    Initial Commit

---
+++ Skills/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skills
+//
+//  Created by Kyle Rohr on 20/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skills/SkillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillsApp.swift
+//  Skills
+//
+//  Created by Kyle Rohr on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SkillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
